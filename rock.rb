efc58396181de5d6de692a5b5bd29f04bea2d449
winderbinder <junior_19000@hotmail.com>
2018-02-28 15:18:37 -0700

    initial commit

---
+++ rock.rb
@@ -0,0 +1,47 @@
+loop do
+
+  options = %w[rock paper scissors lizard spock]
+
+  computer_player = options[rand(options.length)]
+  puts "welcome to rock paper scissors lizard spock! what weapon of destruction shall you choose?"
+  player_input = gets.chomp.strip
+
+  while !(player_input == "rock" || player_input == "paper" || player_input == "scissors" || player_input == "lizard" || player_input == "spock" || player_input == "potato")
+    puts "that weapon does not exist in this world!"
+    puts "pick one of 5 weapons that i have brought upon you!"
+    player_input = gets.chomp.strip
+  end
+
+  if player_input == "rock" || player_input == "paper" || player_input == "scissors" || player_input == "lizard" || player_input == "spock"
+    if player_input == computer_player
+      puts "player and computer tied!"
+    elsif player_input == "rock" && computer_player == "scissors"
+      puts "player chose #{player_input} and computer player chose #{computer_player} player won!"
+    elsif player_input == "rock" && computer_player == "lizard"
+      puts "player chose #{player_input} and computer player chose #{computer_player} player won!"
+    elsif player_input == "paper" && computer_player == "rock"
+      puts "player chose #{player_input} and computer player chose #{computer_player} player won!"
+    elsif player_input == "paper" && computer_player == "spock"
+      puts "player chose #{player_input} and computer player chose #{computer_player} player won!"
+    elsif player_input == "scissors" && computer_player == "lizard"
+      puts "player chose #{player_input} and computer player chose #{computer_player} player won!"
+    elsif player_input == "scissors" && computer_player == "paper"
+      puts "player chose #{player_input} and computer player chose #{computer_player} player won!"
+    elsif player_input == "lizard" && computer_player == "spock"
+      puts "player chose #{player_input} and computer player chose #{computer_player} player won!"
+    elsif player_input == "lizard" && computer_player == "paper"
+      puts "player chose #{player_input} and computer player chose #{computer_player} player won!"
+    elsif player_input == "spock" && computer_player == "rock"
+      puts "player chose #{player_input} and computer player chose #{computer_player} player won!"
+    elsif player_input == "spock" && computer_player == "scissors"
+      puts "player chose #{player_input} and computer player chose #{computer_player} player won!"
+    else
+      puts "player chose #{player_input} and computer player chose #{computer_player} computer player won!"
+    end
+  end
+
+  puts "wanna play again? yes/no"
+  answer = gets.chomp.strip
+  break if answer == "no"
+end
+
