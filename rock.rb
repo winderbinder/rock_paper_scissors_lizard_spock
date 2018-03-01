daf2b1ac6238e3aac8716e61de91f917e40e0595
winderbinder <junior_19000@hotmail.com>
2018-03-01 14:32:56 -0700

    made some refactor changes

--- rock.rb
+++ rock.rb
@@ -1,4 +1,3 @@
-loop do
   puts bigstring = "
  *****    * **     * **   *        * **
  *   **   *    *  *       *       *
@@ -7,61 +6,47 @@ loop do
  *     *  *       * * *   * * **  * * *
 
 "
-  
-
+loop do
   weapons = %w[rock paper scissors lizard spock]
+  acceptable_inputs = (acceptable_inputs == "rock" || acceptable_inputs == "paper" || acceptable_inputs == "scissors" || acceptable_inputs == "lizard" || acceptable_inputs == "spock" || acceptable_inputs == "potato" || acceptable_inputs == "ar15")
+
 
   computer_input = weapons.sample
   puts "welcome to rock paper scissors lizard spock! what weapon of destruction shall you choose?"
-  player_input = gets.chomp.strip
+  acceptable_inputs = gets.chomp.strip
 
-  while !(player_input == "rock" || player_input == "paper" || player_input == "scissors" || player_input == "lizard" || player_input == "spock" || player_input == "potato" || player_input == "ar15")
+  while !(acceptable_inputs)
     puts "that weapon does not exist in this world!"
     puts "pick one of 5 weapons that i have brought upon you!"
-    player_input = gets.chomp.strip
+    acceptable_inputs = gets.chomp.strip
   end
 
-  if player_input == "rock" || player_input == "paper" || player_input == "scissors" || player_input == "lizard" || player_input == "spock" || player_input == "potato" || player_input == "ar15"
-    if player_input == computer_input
+  if acceptable_inputs
+    if acceptable_inputs == computer_input
       puts "player and computer tied!"
-    elsif player_input == "rock" && computer_input == "scissors"
-      puts "player chose #{player_input} and computer player chose #{computer_input} player WON!!"
+    elsif acceptable_inputs == "rock" && (computer_input == "scissors" || computer_input == "lizard")
+      puts "player chose #{acceptable_inputs} and computer player chose #{computer_input} player WON!!"
 
-    elsif player_input == "rock" && computer_input == "lizard"
-      puts "player chose #{player_input} and computer player chose #{computer_input} player WON!!"
+    elsif acceptable_inputs == "paper" && (computer_input == "rock" || computer_input == "spock")
+      puts "player chose #{acceptable_inputs} and computer player chose #{computer_input} player WON!!"
 
-    elsif player_input == "paper" && computer_input == "rock"
-      puts "player chose #{player_input} and computer player chose #{computer_input} player WON!!"
+    elsif acceptable_inputs == "scissors" && (computer_input == "lizard" || computer_input == "paper")
+      puts "player chose #{acceptable_inputs} and computer player chose #{computer_input} player WON!!"
 
-    elsif player_input == "paper" && computer_input == "spock"
-      puts "player chose #{player_input} and computer player chose #{computer_input} player WON!!"
+    elsif acceptable_inputs == "lizard" && (computer_input == "spock" || computer_input == "paper")
+      puts "player chose #{acceptable_inputs} and computer player chose #{computer_input} player WON!!"
 
-    elsif player_input == "scissors" && computer_input == "lizard"
-      puts "player chose #{player_input} and computer player chose #{computer_input} player WON!!"
+    elsif acceptable_inputs == "spock" && (computer_input == "rock" || computer_input == "scissors")
+      puts "player chose #{acceptable_inputs} and computer player chose #{computer_input} player WON!!"
 
-    elsif player_input == "scissors" && computer_input == "paper"
-      puts "player chose #{player_input} and computer player chose #{computer_input} player WON!!"
-
-    elsif player_input == "lizard" && computer_input == "spock"
-      puts "player chose #{player_input} and computer player chose #{computer_input} player WON!!"
-
-    elsif player_input == "lizard" && computer_input == "paper"
-      puts "player chose #{player_input} and computer player chose #{computer_input} player WON!!"
-
-    elsif player_input == "spock" && computer_input == "rock"
-      puts "player chose #{player_input} and computer player chose #{computer_input} player WON!!"
-
-    elsif player_input == "spock" && computer_input == "scissors"
-      puts "player chose #{player_input} and computer player chose #{computer_input} player WON!!"
-
-    elsif player_input == "potato"
+    elsif acceptable_inputs == "potato"
       puts "ARMAGEDDON HAS ARRIVED COMPUTER PLAYER LOSES!!!"
 
-    elsif player_input == "ar15"
+    elsif acceptable_inputs == "ar15"
       puts "yaargghh! gun control!"
 
     else
-      puts "player chose #{player_input} and computer player chose #{computer_input} player LOST!!"
+      puts "player chose #{acceptable_inputs} and computer player chose #{computer_input} player LOST!!"
     end
   end
 
